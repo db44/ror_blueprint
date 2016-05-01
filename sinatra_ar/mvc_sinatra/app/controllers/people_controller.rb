4a26527df02ffb89704a5a38a53b9f9b662c53c2
David Barry <davidbarry2020@gmail.com>
2016-05-01 12:56:37 -0700

    204 lesson 10, edit a person

---
+++ sinatra_ar/mvc_sinatra/app/controllers/people_controller.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+
+get '/people' do
+	@people = Person.all
+  erb :"/people/index"
+end
+
+get '/people/new' do
+  @person = Person.new	
+  erb :"/people/new"
+end	
+
+post '/people' do
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+
+person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: params[:birthdate])
+redirect "/people/#{person.id}"
+#erb :"/people/new"
+end  
+
+get '/people/:id/edit' do
+  @person = Person.find(params:[:id])
+  erb :'/people/edit'
+end
+
+post '/people/:id/edit' do
+# get the record and update the first_name and last_name here...
+  person.birthdate = params[:birthdate]
+  person.save
+  redirect "/people/#{person.id}"
+end
+
+get '/people/:id' do
+  @person = Person.find(params[:id])
+  birthdate_string = @person.birthdate.strftime("%m%d%Y")
+  birth_path_num = Person.get_birth_path_num(birthdate_string)
+  @message = Person.get_message(birth_path_num)	
+  erb :"/people/show"
+end
